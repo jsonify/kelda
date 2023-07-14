f6e376305c044f834481b31e1dcbd4b88aad0457
Jason <phlacin@gmail.com>
2023-07-13 17:32:58 -0700

    initial commit

---
+++ player/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var speed := 35
+
+func handleInput():
+	var moveDirection = Input.get_vector("left","right","up","down")
+	velocity = moveDirection * speed
+	
+
+func _physics_process(delta):
+	handleInput()
+	move_and_slide()
